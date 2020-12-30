f336e67e8e8baacbfc778dd970537ccd5ce03bc9
f3d0t <logrus7@gmail.com>
2020-12-30 14:39:11 +0300

    js pre oop

--- index.js
+++ index.js
@@ -7,8 +7,47 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-
-
+const dog = {
+	species: "dog",
+	name: "Toby",
+	gender: "male",
+	legs: 4,
+	hands: 0,
+	saying: "BaRk!!",
+};
+const cat = {
+	species: "cat",
+	name: "Tima",
+	gender: "male",
+	legs: 4,
+	hands: 0,
+	saying: "Meooow!!",
+};
+const woman = {
+	species: "human",
+	name: "Mella",
+	gender: "female",
+	legs: 2,
+	hands: 2,
+	saying: "Okay honey",
+};
+const man = {
+	species: "human",
+	name: "Antonio",
+	gender: "male",
+	legs: 2,
+	hands: 2,
+	saying: "Yabba dabba doo",
+};
+const catWoman = {
+	species: "catHuman",
+	name: "Selina",
+	gender: "female",
+	legs: 2,
+	hands: 2,
+	saying: cat.saying,
+};
+const inhabitants = [dog, cat, man, woman, catWoman];
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -17,7 +56,9 @@
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
-
+inhabitants.forEach((being) => {
+	print([being.species, being.name, being.jender, being.legs, being.hands, being.saying].join("; ") + "\n");
+});
 /* Print examples:
    print('ABC');
    print('<strong>ABC</strong>');
@@ -27,5 +68,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
